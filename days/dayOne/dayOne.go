f8ef3ae56010d5b417efeb69cd9ba099b5e25796
Barsh <barsha.richard@gmail.com>
2022-12-22 20:22:34 +1100

    cleaned up day 1

--- days/dayOne/dayOne.go
+++ days/dayOne/dayOne.go
@@ -12,8 +12,6 @@ import (
 const inputFilePath = "./days/dayOne/input.txt"
 
 func Run() {
-	fmt.Println("Hello its me - day 1 :)")
-
 	readFile, err := os.Open(inputFilePath)
 
 	if err != nil {
@@ -25,18 +23,12 @@ func Run() {
 
 	fileScanner := bufio.NewScanner(readFile)
 
-	currMax := 0
 	currTotal := 0
 	var totals []int
 
 	for fileScanner.Scan() {
 		if line := fileScanner.Text(); line == "" || line == "\n" {
-			// an empty string is found, we have totalled a group.
-			// add each group total to an array and sort to find the top 3
 			totals = append(totals, currTotal)
-			if currTotal > currMax {
-				currMax = currTotal
-			}
 			currTotal = 0
 		} else {
 			if val, err := strconv.Atoi(line); err == nil {
@@ -44,16 +36,12 @@ func Run() {
 			}
 
 		}
-		if currTotal > currMax {
-			currMax = currTotal
-		}
 	}
 
 	sort.Ints(totals)
 	if len(totals) > 3 {
 		fmt.Printf("Top 3 totals - %v, %v, %v", totals[len(totals)-1], totals[len(totals)-2], totals[len(totals)-3])
-		fmt.Printf("\nGrant Total of Top 3 - %v\n", totals[len(totals)-1]+totals[len(totals)-2]+totals[len(totals)-3])
+		fmt.Printf("\nGrand Total of Top 3 - %v\n", totals[len(totals)-1]+totals[len(totals)-2]+totals[len(totals)-3])
 	}
 
-	// fmt.Println(currMax)
 }
